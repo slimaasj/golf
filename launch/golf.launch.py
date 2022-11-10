505ff86627675e618cea5fc5171c8f90870e037a
slimaasj <salimjbara@gmail.com>
2022-11-10 15:20:01 -0500

    Add files via upload

--- launch/golf.launch.py
+++ launch/golf.launch.py
@@ -6,19 +6,13 @@ from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='golf').find('golfball_robot')
-    default_model_path = os.path.join(pkg_share, 'src/models/golfball_robot.urdf')
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='golf').find('golf')
+    default_model_path = os.path.join(pkg_share, 'src/golf/models/golfball_robot.urdf')
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
-    )
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
     robot_localization_node = launch_ros.actions.Node(
         package='robot_localization',
@@ -34,8 +28,6 @@ def generate_launch_description():
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
                                             description='Flag to enable use_sim_time'),
-        joint_state_publisher_node,
-        robot_state_publisher_node,
         robot_state_publisher_node,
         robot_localization_node,
 
