8c8cf0407eedc82b7afaa9c065fc4a5905147cbd
slimaasj <salimjbara@gmail.com>
2022-11-10 14:48:48 -0500

    Add files via upload

--- launch/golf.launch.py
+++ launch/golf.launch.py
@@ -6,8 +6,8 @@ from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='sam_bot_description').find('sam_bot_description')
-    default_model_path = os.path.join(pkg_share, 'src/description/sam_bot_description.urdf')
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='golf').find('golfball_robot')
+    default_model_path = os.path.join(pkg_share, 'src/models/golfball_robot.urdf')
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
@@ -30,8 +30,6 @@ def generate_launch_description():
 
 
     return LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                            description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
