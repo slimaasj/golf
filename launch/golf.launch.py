445841be7adf30fd7686d45d43a6ffeaecb3c768
slimaasj <salimjbara@gmail.com>
2022-11-15 15:57:38 -0500

    Add files via upload

--- launch/golf.launch.py
+++ launch/golf.launch.py
@@ -14,12 +14,19 @@ def generate_launch_description():
         executable='robot_state_publisher',
         parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
-    robot_localization_node = launch_ros.actions.Node(
+    robot_localization_node_map = launch_ros.actions.Node(
         package='robot_localization',
         executable='ekf_node',
         name='ekf_filter_node',
         output='screen',
-        parameters=[os.path.join(pkg_share, 'ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        parameters=[os.path.join(pkg_share, 'ekf_map.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    )
+    robot_localization_node_odom = launch_ros.actions.Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=[os.path.join(pkg_share, 'ekf_odom.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
 
 
@@ -29,7 +36,8 @@ def generate_launch_description():
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
                                             description='Flag to enable use_sim_time'),
         robot_state_publisher_node,
-        robot_localization_node,
+        robot_localization_node_map,
+        robot_localization_node_odom,
 
         Node(
             package='golf',
